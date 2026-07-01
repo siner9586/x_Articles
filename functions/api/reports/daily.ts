9b1092b280a1c7f3569f5b9773a65c127dc74a3c
siner9586 <47667586+siner9586@users.noreply.github.com>
2026-07-02 00:36:01 +0800

    refactor: remove daily report api route

--- functions/api/reports/daily.ts
+++
@@ -1,4 +0,0 @@
-import { ok } from '../../../src/lib/api';
-import { generateIssueReport } from '../../../src/lib/reports';
-
-export const onRequestGet: PagesFunction = async () => ok(generateIssueReport());
